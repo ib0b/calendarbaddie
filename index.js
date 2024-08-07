ecaf64755e847bca655396d411bc056a92154d89
Bob Kimani <kbob@google.com>
2024-08-07 13:54:22 +0000

    Create skeleton

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * This is the main funciton in this app.
+ * Assume this function is the main entry point of the application
+ * It has the following parts
+ * 1. Email Parsing: Get emails and conver to array of [{subject,content},..]
+ * 2. Gemini Ingestion : Use the array and promptGemini to get an arry of [{tilte, duraion},...]
+ * 3. Calendar Event Createor: Use the output to create calendar events 
+ */
+const main = async ()=>{
+     //const emails = await getUserEmails()
+     //const events = await getUseEvents()
+     //const {success,error} = await createCalendar events()
+    //  if(error){
+    //     console.log(error)
+    //  }
+}
