6692f8b876bc0b69f3166aaba29b4b9f8cad0b4b
Harold Nyikal <nyikal@google.com>
2024-08-08 11:18:29 +0300

    Refactor out gmail auth functions for reuse

---
+++ gmail/_testGmail.js
@@ -0,0 +1,3 @@
+const { listLabels } = require('./actions')
+
+listLabels().catch(console.error);
--- gmail/auth.js
+++ gmail/auth.js
@@ -65,25 +65,8 @@ async function authorize() {
     return client;
 }
 
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listLabels(auth) {
-    const gmail = google.gmail({version: 'v1', auth});
-    const res = await gmail.users.labels.list({
-        userId: 'me',
-    });
-    const labels = res.data.labels;
-    if (!labels || labels.length === 0) {
-        console.log('No labels found.');
-        return;
-    }
-    console.log('Labels:');
-    labels.forEach((label) => {
-        console.log(`- ${label.name}`);
-    });
-}
+module.exports = authorize
 
-authorize().then(listLabels).catch(console.error);
+
+
+
